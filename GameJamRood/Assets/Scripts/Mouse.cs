a2a3bfcb07a045a0602a53cc06cb3f3f95003dfd
JinSjauw <jinzhouliu4@gmail.com>
2021-09-03 14:07:58 +0200

    Mouse Clicking

---
+++ GameJamRood/Assets/Scripts/Mouse.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+        
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //Debug.Log(mousePos.x);
+            //Debug.Log(mousePos.y);
+
+            if(Physics.Raycast(Ray, out hit)) 
+            {
+                Debug.DrawRay(mousePos, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+                Debug.Log("Shot ray");
+                Debug.Log("Tag:" + hit.collider.tag);
+
+
+                if (hit.collider.tag == "Enemy") 
+                {
+
+                    //hit.collider.gameObject.SetActive(false);
+                    Destroy(hit.collider.gameObject);
+
+
+                }
+
+
+
+            }
+            //Debug.DrawRay(mousePos, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+
+            Debug.Log(mousePos.x);
+            Debug.Log(mousePos.y);
+
+        }
+    }
+}
